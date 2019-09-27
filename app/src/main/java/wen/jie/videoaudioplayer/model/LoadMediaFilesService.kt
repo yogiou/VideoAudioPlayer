8447b474142af790d0749e72bb63936661044a39
Jie Wen <jie.wen@capgemini.com>
2019-09-27 23:11:16 +0800

    bug fix

--- app/src/main/java/wen/jie/videoaudioplayer/model/LoadMediaFilesService.kt
+++ app/src/main/java/wen/jie/videoaudioplayer/model/LoadMediaFilesService.kt
@@ -7,29 +7,28 @@ import android.provider.MediaStore
 
 
 class LoadMediaFilesService {
-    fun loadAllMediaFiles(contentResolver: ContentResolver): MutableList<File> {
-        val fileList = ArrayList<File>()
-
-        val audioProj = arrayOf(MediaStore.Audio.Media._ID, MediaStore.Audio.Media.DISPLAY_NAME)
-        val videoProj = arrayOf(MediaStore.Video.Media._ID, MediaStore.Video.Media.DISPLAY_NAME)
-
-        val audioCursor = contentResolver.query(
-            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
-            audioProj,
-            null,
-            null,
-            null
-        )
-
-        audioCursor?.let {
-            if (audioCursor.moveToFirst()) {
-                do {
-                    val audioIndex =
-                        audioCursor.getColumnIndexOrThrow(MediaStore.Audio.Media.)
-
-                    fileList.add(audioCursor.getString(audioIndex))
-                } while (audioCursor.moveToNext())
-            }
-        }
-    }
+//    fun loadAllMediaFiles(contentResolver: ContentResolver): MutableList<File> {
+//        val fileList = ArrayList<File>()
+//
+//        val audioProj = arrayOf(MediaStore.Audio.Media._ID, MediaStore.Audio.Media.DISPLAY_NAME)
+//        val videoProj = arrayOf(MediaStore.Video.Media._ID, MediaStore.Video.Media.DISPLAY_NAME)
+//
+//        val audioCursor = contentResolver.query(
+//            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+//            audioProj,
+//            null,
+//            null,
+//            null
+//        )
+//
+//        audioCursor?.let {
+//            if (audioCursor.moveToFirst()) {
+//                do {
+////                    val audioIndex = audioCursor.getColumnIndexOrThrow(MediaStore.Audio.Media.)
+////
+////                    fileList.add(audioCursor.getString(audioIndex))
+//                } while (audioCursor.moveToNext())
+//            }
+//        }
+//    }
 }
